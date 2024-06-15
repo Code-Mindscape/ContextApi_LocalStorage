dc658c0b5ca94c33974626255b0652a102d62ac7
Mirza Usman <mirzausmanmu07842@gmail.com>
2024-06-15 20:04:22 +0500

    added functionalties to mainpulate todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { TodoProvider } from "./contexts/TodoContext";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) =>{
+    setTodos((prev)=> prev.map((prevTodo)=> (prevTodo.id === id)? prevTodo.todo === todo : prevTodo ))
+  }
+
+  const deleteTodo = (id)=>{
+    setTodos((prev)=> prev.filter((CurrentTodo)=> ( CurrentTodo.id !== id ) ))
+  }
+
+  const toggleTodo = (id)=>{
+    setTodos((prev)=> prev.map((todo)=> (todo.id === id )? !(todo.iscompleted) : todo ))
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <TodoProvider
+      value={{ addTodo, deleteTodo, todos, toggleTodo, updateTodo }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </TodoProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import React, { useContext, createContext } from "react";
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo: "this is a todo task",
+            iscompleted: false
+        }
+    ],
+    addTodo: (todo)=>{},
+    updateTodo: (id, todo)=>{},
+    deleteTodo: (id)=>{},
+    toggleTodo: (id)=>{}
+});
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+};
+
+export const TodoProvider = TodoContext.Provider;
